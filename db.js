6497fbab09ed3c3fa563b6d670120bd5d8fbb434
Derek Black <derekblack94@yahoo.com>
2021-01-12 16:34:25 -0500

    firstCommit

--- db.js
+++ db.js
@@ -1,24 +1,40 @@
-const Sequelize = require('sequelize');
-// const UserBike = database.import('./models/userbike');
-// const User = database.import('./models/user');
-// const Appointments = database.import('./models/appointments')
+// const Sequelize = require('sequelize');
+// // const UserBike = database.import('./models/userbike');
+// // const User = database.import('./models/user');
+// // const Appointments = database.import('./models/appointments')
 
-const sequelize = new Sequelize( process.env.DATABASE_URL, {
+// const sequelize = new Sequelize( process.env.DATABASE_URL, {
 
     
+//     dialect: 'postgres'
+// });
+
+// // sequelize.authenticate().then(
+// //     () => {
+// //         console.log('Connected to Red-Badge-Server database');
+// //     },
+// //     (err) => {
+// //         console.log(err);
+// //     }
+// // );
+
+
+
+// module.exports = sequelize;
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
-// sequelize.authenticate().then(
-//     () => {
-//         console.log('Connected to Red-Badge-Server database');
-//     },
-//     (err) => {
-//         console.log(err);
-//     }
-// );
-
-
+sequelize.authenticate().then(
+    () => {
+        console.log('Connected to bike shop database');
+    },
+    (err) => {
+        console.log(err);
+    }
+);
 
 module.exports = sequelize;
 
